77e23a25dd1682c2372bda39efd23d2d98e09f71
RatnadeepBiswakarma <36644040+RatnadeepBiswakarma@users.noreply.github.com>
2018-06-02 09:17:04 +0530

    But fixed

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ function calculateMe() {
     var period = document.getElementById("rate-type").value;
     let months = document.getElementById("months").value;
     if ((period === "Monthly") && (totalAmount > 0) && (interestRate > 0)) {
-        document.getElementById("per-months").innerHTML = ((totalAmount * interestRate) / 100);
-        document.getElementById("total-interest").innerHTML = (((totalAmount * interestRate) / 100) * months);
-        document.getElementById("subtotal").innerHTML = parseInt((((totalAmount * interestRate) / 100) * months) + totalAmount);
+        document.getElementById("per-months").innerHTML = ((totalAmount * interestRate) / 100).toFixed(2);
+        document.getElementById("total-interest").innerHTML = (((totalAmount * interestRate) / 100) * months).toFixed(2);
+        document.getElementById("subtotal").innerHTML = ((((totalAmount * interestRate) / 100) * months) + totalAmount).toFixed(2);
     } else {
         document.getElementById("alert").innerHTML = "Please provide above information correctly.";
     }
